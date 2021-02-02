0c8464d62d7d080820ebde13f0e67fe45a3f1771
tradeurcasl <tokareva.spbu@gmail.com>
2021-02-02 18:20:35 +0300

    добавлено несколько тренировочных функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("world");
+
+        double l = 6.0;
+        System.out.println("Площадь квадрата со стороной " + l + "=  " + area(l));
+
+        double a = 7.9;
+        double b = 4;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody);
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
