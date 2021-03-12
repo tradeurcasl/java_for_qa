ec372d781831b24713102db665303fd251e3a1bd
tradeurcasl <tokareva.spbu@gmail.com>
2021-03-12 09:35:39 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге
    +модульные классы для них

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+
+    public ContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+    public String getMiddlename() {
+        return middlename;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public String getAddress() {
+        return address;
+    }
+}
