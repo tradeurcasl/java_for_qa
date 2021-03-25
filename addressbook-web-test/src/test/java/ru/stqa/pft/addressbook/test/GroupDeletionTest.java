e0ad8fc921be7c52f7a1a5260206a371ab931c40
tradeurcasl <tokareva.spbu@gmail.com>
2021-03-25 13:12:18 +0300

    Лекция 5.2. Улучшение кода (группы)

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTest.java
@@ -5,23 +5,26 @@ import org.testng.annotations.Test;
 import org.testng.Assert;
 import java.util.List;
 import java.util.Comparator;
-
+import org.testng.annotations.BeforeMethod;
 
 public class GroupDeletionTest extends TestBase {
 
-    @Test
-    public void testGroupDeletion() {
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData(8,"Test1", null, null));
         }
+    }
+    @Test
+    public void testGroupDeletion() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
-        app.getNavigationHelper().returnToHomePage();
+        //app.getNavigationHelper().returnToHomePage();
         before.remove(before.size() - 1);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
@@ -4,28 +4,27 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import java.util.List;
-import java.util.HashSet;
+import org.testng.annotations.BeforeMethod;
 import java.util.Comparator;
 
 
 public class GroupModificationTests extends TestBase {
 
-    @Test
-    public void testGroupModification() {
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData(0,"Test1", null, null));
-        }
+        }}
+    @Test
+    public void testGroupModification() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1);
-        app.getGroupHelper().initGroupModification();
-        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"Test1", "test2", "Test3");
-        app.getGroupHelper().fillGroupForm(group);
-        app.getGroupHelper().submitGroupModification();
-        app.getNavigationHelper().gotoGroupPage();
+        int index = before.size() - 1;
+        GroupData group = new GroupData(before.get(index).getId(),"Test1", "test2", "Test3");
+        app.getGroupHelper().modifyGroup(index, group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
-        before.remove(before.size() - 1);
+        before.remove(index);
         before.add(group);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
