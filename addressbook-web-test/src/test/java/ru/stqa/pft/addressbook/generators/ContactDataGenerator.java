368f7662603dda1a7e7391af440cacd6bba524ec
tradeurcasl <tokareva.spbu@gmail.com>
2021-03-29 15:40:45 +0300

    Лекция 6.3. Анализ опций командой строки при запуске генератора данных

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -4,6 +4,9 @@ package ru.stqa.pft.addressbook.generators;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,12 +15,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
         List<ContactData> contacts = generateContacts(count);
-        save(contacts, file);
+        save(contacts, new File(file));
     }
 
     private static void save(List<ContactData> contacts, File file) throws IOException {
@@ -29,7 +48,7 @@ public class ContactDataGenerator {
         writer.close();
     }
 
-    private static List<ContactData> generateContacts(int count) {
+    private List<ContactData> generateContacts(int count) {
         List<ContactData>contacts = new ArrayList<ContactData>();
         for (int i = 0; i <count; i++){
             contacts.add(new ContactData().withLastname(String.format("lastname %s", i)).withFirstname(String.format("firstname %s", i)).withAddress(String.format("address %s", i)));
